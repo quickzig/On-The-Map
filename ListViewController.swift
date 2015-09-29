a9724b8975637e9058d13ae91ed042abf4571b73
Quickzig <brian.quick@gmail.com>
2015-09-28 22:37:22 -0500

    More Updates

---
+++ ListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ListViewController.swift
+//  On The Map
+//
+//  Created by Brian Quick on 9/28/15.
+//  Copyright © 2015 Brian Quick. All rights reserved.
+//
+
+import UIKit
+
+
+class ListViewController: UITableViewController {}
---
+++ MapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewController.swift
+//  On The Map
+//
+//  Created by Brian Quick on 9/28/15.
+//  Copyright © 2015 Brian Quick. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController:  UIViewController, MKMapViewDelegate {
+    
+}
