fd4a4bc41d5e65c18ddf8a845b512e24606e2790
Quickzig <brian.quick@gmail.com>
2015-09-30 10:57:27 -0500

    New View

---
+++ InfoPostingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  InfoPostingViewController.swift
+//  On The Map
+//
+//  Created by Brian Quick on 9/30/15.
+//  Copyright © 2015 Brian Quick. All rights reserved.
+//
+
+
+import UIKit
+
+class InfoPostingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+        
+    
+}
+
